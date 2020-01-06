b04617e5fff6750874ad9ff1e4430609bd28bb90
EmmaYu <limit38317@gmail.com>
2020-01-06 13:21:04 +0900

    test

---
+++ anytest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anytest
+//
+//  Created by 유은영 on 2020/01/03.
+//  Copyright © 2020 유은영. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
